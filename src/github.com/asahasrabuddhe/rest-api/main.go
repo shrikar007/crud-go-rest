9513cdeb922243c7251a5d46849a3583d6eee9a0
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-06-13 17:40:25 +0530

    updated code to use chi router over native http lib

--- src/github.com/asahasrabuddhe/rest-api/main.go
+++ src/github.com/asahasrabuddhe/rest-api/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
 		_, _ = fmt.Fprintf(writer, "Hello, %q", html.EscapeString(request.URL.Path))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }

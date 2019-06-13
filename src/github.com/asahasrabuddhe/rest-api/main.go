0dacf02c06caddfd0646f5063f386caee5fe6caa
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-06-13 18:16:01 +0530

    basic structure of api done, basic get and create implemented

--- src/github.com/asahasrabuddhe/rest-api/main.go
+++ src/github.com/asahasrabuddhe/rest-api/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"html"
+	"google.golang.org/genproto/googleapis/type/date"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Expense struct {
+	Id          int       `json:"id"`
+	Description string    `json:"description"`
+	Type        string    `json:"type"`
+	Amount      float64   `json:"amount"`
+	CreatedOn   date.Date `json:"created_on" `
+	UpdatedOn   date.Date `json:"updated_on"`
+}
+
+type Expenses []Expense
+
+var expenses Expenses
 
 func main() {
 	r := chi.NewRouter()
@@ -17,9 +32,72 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = fmt.Fprintf(writer, "Hello, %q", html.EscapeString(request.URL.Path))
+	r.Route("/expenses", func(r chi.Router) {
+		r.Post("/", CreateExpense)
+		r.Get("/", ListAllExpense)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", ListOneExpense)
+			r.Put("/", UpdateExpense)
+			r.Delete("/", DeleteExpense)
+		})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func CreateExpense(writer http.ResponseWriter, request *http.Request) {
+	b, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		http.Error(writer, "unable to read request body", 500)
+	}
+
+	var data map[string]interface{}
+
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		http.Error(writer, "unable to parse json request body", 422)
+	}
+
+	expense := new(Expense)
+
+	if val, ok := data["description"].(string); ok {
+		expense.Description = val
+	}
+
+	if val, ok := data["type"].(string); ok {
+		expense.Type = val
+	}
+
+	if val, ok := data["amount"].(float64); ok {
+		expense.Amount = val
+	}
+
+	expenses = append(expenses, *expense)
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+
+	_, _ = fmt.Fprintln(writer, `{"success": true}`)
+}
+
+func ListOneExpense(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func ListAllExpense(writer http.ResponseWriter, request *http.Request) {
+	encoder := json.NewEncoder(writer)
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+
+	_ = encoder.Encode(expenses)
+}
+
+func UpdateExpense(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func DeleteExpense(writer http.ResponseWriter, request *http.Request) {
+
 }
